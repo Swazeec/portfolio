7d7a395efb9c0e0f92f33bc3cca4a233d955cc10
Soizic Venel <venel.soizic@hotmail.fr>
2023-03-06 16:08:04 +0100

    start adding content to Project.js

---
+++ src/components/UI/ProjectCard.js
@@ -0,0 +1,22 @@
+import { Col, Card } from "react-bootstrap"
+
+
+const ProjectCard = props => {
+    return (
+        <Col xs={12} md={6} xl={4} className="p-1 m-0">
+                <Card className="h-100">
+                    <Card.Body>
+                        <h3><i className="bi bi-code"></i> {props.title}</h3>
+                        <Card.Img src={props.image}/>
+                        <Card.Text  className="text-center">{props.tech}</Card.Text>
+                        <div className="text-center">
+                            <Card.Link href={props.github} target="blank"><i className="bi bi-github"></i></Card.Link>
+                            <Card.Link href={props.website} target="blank"><i className="bi bi-link-45deg"></i></Card.Link>
+                        </div>
+                    </Card.Body>
+                </Card>
+            </Col>
+    )
+}
+
+export default ProjectCard
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,20 @@
+import { Row } from "react-bootstrap"
+
+import ProjectCard from "../components/UI/ProjectCard"
+
+import countryQuiz from '../assets/projectsPictures/countryQuiz.jpg'
+import adviceGen from '../assets/projectsPictures/adviceGenerator.jpg'
+import timeTrack from '../assets/projectsPictures/timeTrackingBoard.jpg'
 
 const Projects = () =>{
-    return <h2>Projets</h2>
+    return <>
+        <h2>Projets</h2>
+        <Row className="p-0 m-0">
+            <ProjectCard title='Quiz App' image={countryQuiz} tech='React / HTML / CSS / BS / API' github='https://github.com/Swazeec/quizApp' website='https://quiz-countries.netlify.app'/>
+            <ProjectCard title='Advice Generator' image={adviceGen} tech='React / HTML / CSS / API' github='https://github.com/Swazeec/advicegenerator' website='https://small-advice-generator.netlify.app'/>
+            <ProjectCard title='Time Tracking Ap' image={timeTrack} tech='React / HTML / CSS' github='https://github.com/Swazeec/timeTrackingDashboard' website='https://rainbow-valkyrie.netlify.app/'/>
+        </Row>
+    </>
 }
 
 export default Projects
