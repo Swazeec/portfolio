3379eaae7bae0cd7dae8a39503fb72b291953033
Soizic Venel <venel.soizic@hotmail.fr>
2023-03-09 14:45:08 +0100

    modify Socials.js component with right links

--- src/components/Navigation/Socials.js
+++ src/components/Navigation/Socials.js
@@ -1,15 +1,13 @@
-import { NavLink } from "react-router-dom"
-
 import classes from './Socials.module.css'
 
 
 const Socials = () => {
     return (
         <div className={classes.socials}>
-            <NavLink to='https://github.com/Swazeec/'><i className="bi bi-github"></i></NavLink>
-            <NavLink to='https://github.com/Swazeec/'><i className="bi bi-linkedin"></i></NavLink>
-            <NavLink to='https://github.com/Swazeec/'><i className="bi bi-envelope"></i></NavLink>
-            <NavLink to='https://github.com/Swazeec/'><i className="bi bi-phone"></i></NavLink>
+            <a href='https://github.com/Swazeec/'><i className="bi bi-github"></i></a>
+            <a href='https://www.linkedin.com/in/soizic-venel/'><i className="bi bi-linkedin"></i></a>
+            <a href='mailto:venel.soizic@hotmail.fr'><i className="bi bi-envelope"></i></a>
+            <a href='tel:0683781954'><i className="bi bi-phone"></i></a>
         </div>
     )
 }
