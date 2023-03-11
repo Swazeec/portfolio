2d3c21525606bf972abb5a332790d761f96a530d
Soizic Venel <venel.soizic@hotmail.fr>
2023-03-11 16:47:07 +0100

    start adding job descriptions

--- src/components/UI/ListItem.js
+++ src/components/UI/ListItem.js
@@ -1,12 +1,24 @@
+import { useState } from "react"
+
 const ListItem = props => {
+
+    const [showDescription, setShowDescription] = useState(false)
+    const onClickHandler = ()=> {
+        showDescription ? setShowDescription(false) : setShowDescription(true)
+    }
+
+
+    let chevron = !props.description ? '': showDescription ? <span>  <i className="bi bi-chevron-compact-up"></i></span> : <span>  <i className="bi bi-chevron-compact-down"></i></span>
 
     let h3Classes = props.dates ? 'mb-0' : ''
     let pClasses = props.dates ? 'text-secondary mb-1' : ''
+
     return (
        <li>
-            <h3 className={h3Classes}>{props.title}</h3>
+            <h3 className={h3Classes} onClick={onClickHandler}>{props.title}{chevron}</h3>
             <p className={pClasses}>{props.subtitle}</p>
             {props.dates && <p>{props.dates}</p>}
+            {props.description && showDescription && <ul className="mb-4 descList">{props.description.map(item => <li key={props.description.indexOf(item)}>{item}</li>)}</ul>}
        </li>
     )
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,22 @@
 import ListItem from "../components/UI/ListItem"
 import Section from "../components/UI/Section"
 
+
+
 const About = () =>{
+
+    const xp = [
+        {id:1, title:'Assistante Webmaster – Scandinavia Design', subtitle: 'e-commerce bilingue – Gennes (49)', dates:'de juin 2015 à décembre 2019', description:["Rédaction de contenu original bilingue pour un bon référencement naturel (SEO)", "Amélioration de l’expérience et de l’interface utilisateur (UX/UI)",
+        "Mise à jour et optimisation du site et des fiches produit",
+        "Gestion de base de données Prestashop, gestion de stock et commandes fournisseurs internationaux",
+        "Veille concurrentielle, suivi de l’actualité"]},
+        {id:2, title:'Coordinatrice de projet de développement local – association FÉPPAL', subtitle:'Fédération des Éditeurs et Producteurs phonographiques en Pays de la Loire – Angers (49)', dates:'d’octobre 2013 à juin 2014', description:['mise à jour des supports de communication', 'démarchage de partenaires potentiels', "intégration de contenu (Wordpress et plateforme de streaming)", "gestion administrative (adhésions, réunions, animation de réseau, etc.)"]},
+        {id:3, title:'Assistante d’édition – Éditions Henry Lemoine', subtitle: 'Département musique contemporaine – Paris (75)', dates: 'de février 2008 à août 2010', description:['gestion de stocks, commandes fournisseur, suivi livraisons et facturations', 'saisie et mise en forme de documents divers', 'élaboration d’un support print pour la promotion de l’éditeur (catalogue produit)', 'préparation de documents d’archive pour l’impression numérique' ]}
+    ]
+
+    const xpContent = xp.map(item => <ListItem key={item.id} title={item.title} subtitle={item.subtitle} dates={item.dates} description={item.description} />)
+
+
     return ( <>
         <Section title='Présentation'>
             <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. Je suis bien ! In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.</p>
@@ -23,9 +38,7 @@ const About = () =>{
         </Section>
         <Section title='Expériences'>
             <ul>
-                <ListItem title='Assistante Webmaster – Scandinavia Design' subtitle='e-commerce bilingue – Gennes (49)' dates='de juin 2015 à décembre 2019'/>
-                <ListItem title='Coordinatrice de projet de développement local – association FÉPPAL' subtitle='Fédération des Éditeurs et Producteurs phonographiques en Pays de la Loire – Angers (49)' dates='d’octobre 2013 à juin 2014'/>
-                <ListItem title='Assistante d’édition – Éditions Henry Lemoine' subtitle='Département musique contemporaine – Paris (75)' dates='de février 2008 à août 2010'/>
+                {xpContent}
             </ul>
         </Section>
     </>)
